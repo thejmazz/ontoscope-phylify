5c9806954b5cb9457f62c4e186165381cdadf7cf
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-02-22 16:16:06 -0500

    minimal cytoscape.js setup

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log('wheeeeeee')
+// import cytoscape from 'cytoscape'
+const cytoscape = require('cytoscape')
+
+// TODO css..
+document.documentElement.style.height = '100%'
+document.body.style.margin = '0'
+document.body.style.height = '100%'
+
+// Create and append container dom element
+const containerDiv = document.createElement('div')
+// containerDiv.id = 'cy'
+containerDiv.style.height = '100%'
+document.body.appendChild(containerDiv)
+
+console.log(cytoscape)
+
+const cy = cytoscape({
+  container: containerDiv, 
+
+  elements: [ 
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+}) 
