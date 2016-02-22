d2ab4dc5de8498721724c2ec9d9fb86d8786aa80
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-02-22 15:50:57 -0500

    babel+webpack+browserify dev env

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ontoscope-phylify
+
 Phylify module for Ontoscope
+
+## Running
+
+```bash
+$ npm install
+# in one tab
+$ npm start
+# in another tab
+$ npm run serve
+```
+
+The bundle will recompile as you save, and the webpage will then automatically refresh.
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  entry: ['./index.js'],
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'sourcemap',
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel'                                              
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
