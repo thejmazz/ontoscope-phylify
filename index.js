0694c8a1557ab293b7d472863c946b6998d9e916
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-02-22 20:43:54 -0500

    adds vertices to cytoscape in batches of 500

--- index.js
+++ index.js
@@ -7,20 +7,39 @@ const _ = require('highland')
 const cytoscape = require('cytoscape')
 const obo = require('bionode-obo')
 
+let terms = []
+let total = 0
 
-fetch('/ff-phase2-140729.obo')
-  .then( (response) => {
-    if (response.status >= 400) {
-        throw new Error('Bad response from server');
-    }
-       
-    // _(response.text())
-    //   .splitBy('\n')
-    //   .each(s => console.log(s))
+// let smallerTerms
+
+fetch('/ff-phase2-140729.obo').then( (response) => {
+  if (response.status >= 400) {
+      throw new Error('Bad response from server');
+  }
     
-    _(obo.terms(_(response.text())))
-      .each(s => console.log(s))
-  })
+  _(obo.terms(_(response.text())))
+    .each( (term) => {
+      // console.log(term)
+      // terms.push({data: {id: term.id}})
+      // console.log(terms)
+      if (terms.length <= 500) {
+        terms.push({data: {id: term.id}})
+      }
+      
+      if (terms.length === 500) {
+        // console.log(terms)
+        console.log(total)
+        total += terms.length
+
+        cy.add(terms)
+        let layout = window.cy.elements().makeLayout({name: 'grid'})
+        layout.run()
+
+        terms = []
+      }
+    })
+    .done( () => console.log('done') )
+})
 
 
 // TODO css..
@@ -33,6 +52,7 @@ const containerDiv = document.createElement('div')
 // containerDiv.id = 'cy'
 containerDiv.style.height = '100%'
 document.body.appendChild(containerDiv)
+
 
 
 const cy = cytoscape({
@@ -74,4 +94,10 @@ const cy = cytoscape({
     name: 'grid',
     rows: 1
   }
-}) 
+})
+window.cy = cy
+
+window.cy.add({data: { id: 'c' } })
+window.cy.add({data: { id: 'd'} })
+let layout = window.cy.elements().makeLayout({name: 'grid'})
+layout.run()
