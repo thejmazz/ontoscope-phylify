21c8c46f50df7065574810e46ab4d8c11de6ed5f
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-02-27 00:34:17 -0500

    cleaning and comments

--- igraph/graphed.R
+++ igraph/graphed.R
@@ -1,30 +1,52 @@
-# Packages
+# === Packages ===
+
+# json <-> data frames (w/ streams too)
 if (!require(jsonlite, quietly=TRUE)) {
     install.packages("jsonlite")
 }
 
+# R bindings to C based network analysis package igraph
 if (!require(igraph, quietly=TRUE)) {
     install.packages("igraph")
 }
 
-# ndjson to data frame
-# TODO open/close file nicely
-terms <- stream_in(file("fantom.ndjson", "r"))
+# === Utility Functions ===
 
-# trim whitespace utility function
-# thanks f3lix http://stackoverflow.com/a/2261149/1409233
+# trim whitespace on edges of string
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 
-# Make vector of parsed terms$is_a
-isases <- c()
-# for (i in 1:length(terms$is_a)) { isases <- c(isases, trim(strsplit(terms$is_a[i], "!")[[1]][1]) ) }
+# === Parsing ===
 
+# obo -> ndjson performed externally by bionode-obo
+# see: https://github.com/bionode/bionode-obo
+# why JS? works in the browser as well.
+
+# ndjson -> data frame
+# TODO reference file in some less sketch way -> CLI params
+# TODO option for url streams, pipe, etc.
+inputFile <- "fantom.ndjson"
+conn <- file(inputFile)
+
+# jsonlite#stream_in will handle closing file connection
+terms <- stream_in(conn)
+
+# === Building an igraph object ===
+
+# Edges live as two vectors s,t, where |s| = |t|
+# s[i] -> t[i]
 sources <- c()
 targets <- c()
+
+# Loop over each term
+# source is term$id, target is term$is_a
+# (after removing comments and trimming)
 for (i in 1:length(terms$is_a)) {
     src <- trim(strsplit(terms$is_a[i], "!")[[1]][1])
    
+    # TODO check if validity of target based off target id present
+    # in vector of ids.
+
     # sketchily assume source is a valid id if it contains : or _ 
     validSrc <- grepl(":|_", src)
     
@@ -39,12 +61,16 @@ for (i in 1:length(terms$is_a)) {
     }
 }
 
-# length(sources)
-# length(targets)
-
+# One way to construct an igraph is with
+# igraph#graph_from_data_frame
+# which can take a d.f. with vectors named from, to, for the edges as described
+# above
 relations <- data.frame(from=sources, to=targets)
-ids <- terms$id
-g <- graph_from_data_frame(relations, directed=TRUE, vertices=ids)
+# ids <- terms$id
+g <- graph_from_data_frame(relations, directed=TRUE, vertices=terms$id)
+
+
+# === Plotting ===
 
 # source and do this yourself, heavy atm
 # plot(g)
